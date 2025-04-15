6d18be9c07a07d0a7463c66134e5a084f8aa8f68
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-16 02:30:52 +0530

    Fix formatting by adding new lines for better readability

--- incex.js
+++ incex.js
@@ -1,6 +1,7 @@
 const add = (num1, num2) => {
     return num1 + num2;
 };
+
 const subtract = (num1, num2) => {
     return num1 - num2;
 };
@@ -8,6 +9,7 @@ const subtract = (num1, num2) => {
 const multiply = (num1, num2) => {
     return num1 * num2;
 };
+
 const divide = (num1, num2) => {
     return num1 / num2;
 };
