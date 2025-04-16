0f09ab5f32ec7a04931dc8af5802fdba2a21f70f
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-17 00:15:39 +0530

    Refactor calculator.mjs to use named exports for individual functions

--- calculator.mjs
+++ calculator.mjs
@@ -1,25 +1,25 @@
-export const add = (num1, num2) => {
+const add = (num1, num2) => {
     return num1 + num2;
 };
 
-export const subtract = (num1, num2) => {
+const subtract = (num1, num2) => {
     return num1 - num2;
 };
 
-export const multiply = (num1, num2) => {
+const multiply = (num1, num2) => {
     return num1 * num2;
 };
 
-export const divide = (num1, num2) => {
+const divide = (num1, num2) => {
     return num1 / num2;
 };
 
-// export default {
-//     add,
-//     subtract,
-//     multiply,
-//     divide
-// };
+export default {
+    add,
+    subtract,
+    multiply,
+    divide
+};
 
 // module.exports = {
 //     add: add,
