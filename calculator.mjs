e751ff739a5097dd36c4fcb6c1f33d0adeb3bafb
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-16 23:36:56 +0530

    Implement calculator functions and module exports in calculator.mjs

---
+++ calculator.mjs
@@ -0,0 +1,29 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
+
+export default {
+    add,
+    subtract,
+    multiply,
+    divide
+};
+
+module.exports = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide
+};
